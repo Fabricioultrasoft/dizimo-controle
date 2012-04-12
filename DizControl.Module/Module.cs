90235ed1b48cce44c3c356a04838f1179478d259
jeysson.paiva@funnysys.com <jeysson.paiva@funnysys.com@a3a5e11d-64c1-dcff-e9d1-4ec58fe48fda>
2012-04-12 16:51:54 +0000

    criando trunk

---
+++ DizControl.Module/Module.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+using DevExpress.ExpressApp;
+using System.Reflection;
+
+
+namespace DizControl.Module
+{
+    public sealed partial class DizControlModule : ModuleBase
+    {
+        public DizControlModule()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DizControl.Win/WinApplication.Designer.cs
@@ -0,0 +1,92 @@
+namespace DizControl.Win
+{
+    partial class DizControlWindowsFormsApplication
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule();
+            this.module3 = new DizControl.Module.DizControlModule();
+            this.module4 = new DizControl.Module.Win.DizControlWindowsFormsModule();
+            this.module5 = new DevExpress.ExpressApp.Validation.ValidationModule();
+            this.module6 = new DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule();
+            this.module7 = new DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule();
+
+            this.securityModule1 = new DevExpress.ExpressApp.Security.SecurityModule();
+            this.securitySimple = new DevExpress.ExpressApp.Security.SecurityStrategySimple();
+            this.authenticationActiveDirectory1 = new DevExpress.ExpressApp.Security.AuthenticationActiveDirectory();
+            this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // securitySimple1
+            // 
+            this.securitySimple.Authentication = this.authenticationActiveDirectory1;
+            this.securitySimple.UserType = typeof(DevExpress.ExpressApp.Security.SecuritySimpleUser);
+            // 
+            // authenticationActiveDirectory1
+            // 
+            this.authenticationActiveDirectory1.CreateUserAutomatically = true;
+            this.authenticationActiveDirectory1.UserType = typeof(DevExpress.ExpressApp.Security.SecuritySimpleUser);
+            // 
+            // sqlConnection1
+            // 
+            this.sqlConnection1.ConnectionString = "Data Source=(local);Initial Catalog=DizControl;Integrated Security=SSPI;Pooling=false";
+            this.sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // DizControlWindowsFormsApplication
+            // 
+            this.ApplicationName = "DizControl";
+            this.Connection = this.sqlConnection1;
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module3);
+            this.Modules.Add(this.module4);
+            this.Modules.Add(this.module5);
+            this.Modules.Add(this.module6);
+            this.Modules.Add(this.module7);
+            this.Modules.Add(this.securityModule1);
+            this.Security = this.securitySimple;
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.DizControlWindowsFormsApplication_DatabaseVersionMismatch);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule module2;
+        private DizControl.Module.DizControlModule module3;
+        private DizControl.Module.Win.DizControlWindowsFormsModule module4;
+        private DevExpress.ExpressApp.Validation.ValidationModule module5;
+        private DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule module6;
+        private DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule module7;
+        private DevExpress.ExpressApp.Security.SecurityModule securityModule1;
+        private DevExpress.ExpressApp.Security.SecurityStrategySimple securitySimple;
+        private DevExpress.ExpressApp.Security.AuthenticationActiveDirectory authenticationActiveDirectory1;
+        private System.Data.SqlClient.SqlConnection sqlConnection1;
+    }
+}
