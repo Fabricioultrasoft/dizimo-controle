2eb6714c2078a47116214f22af20efa071b9c6f1
jeysson.paiva@funnysys.com <jeysson.paiva@funnysys.com@a3a5e11d-64c1-dcff-e9d1-4ec58fe48fda>
2012-04-12 16:56:22 +0000

    criando branche na versão 1.0.0a1

---
+++ branches/1.0.0a1/DizControl.Module/App/Movimento/Domain/CsTipoLogradouro.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace DizControl.Module
+{
+    /// <summary>
+    /// Tipo do endereço
+    /// </summary>
+    public enum CsTipoLogradouro
+    {
+        [Description("Aeroporto")]
+        Aeroporto   = 0,
+        [Description("Alameda")]
+        Alameda     = 1,
+        [Description("Área")]
+        Area        = 2,
+        [Description("Avenida")]
+        Avenida     = 3,
+        [Description("Campo")]
+        Campo       = 4,
+        [Description("Chácara")]
+        Chacara     = 5,
+        [Description("Avenida")]
+        Colonia     = 6,
+        [Description("Condomínio")]
+        Condominio  = 7,
+        [Description("Conjunto")]
+        Conjunto    = 8,
+        [Description("Distrito")]
+        Distrito    = 9,
+        [Description("Esplanada")]
+        Esplanada   = 10,
+        [Description("Estação")]
+        Estacao     = 11,
+        [Description("Estrada")]
+        Estrada     = 12,
+        [Description("Favela")]
+        Favela      = 13,
+        [Description("Fazenda")]
+        Fazenda     = 14,
+        [Description("Feira")]
+        Feira       = 15,
+        [Description("Jardim")]
+        Jardim      = 16,
+        [Description("Ladeira")]
+        Ladeira     = 17,
+        [Description("Lago")]
+        Lago        = 18,
+        [Description("Lagoa")]
+        Lagoa       = 19,
+        [Description("Largo")]
+        Largo       = 20,
+        [Description("Loteamento")]
+        Loteamento  = 21,
+        [Description("Morro")]
+        Morro       = 22,
+        [Description("Núcleo")]
+        Nucleo      = 23,
+        [Description("Parque")]
+        Parque      = 24,
+        [Description("Passarela")]
+        Passarela   = 25,
+        [Description("Pátio")]
+        Patio       = 26,
+        [Description("Praça")]
+        Praca       = 27,
+        [Description("Quadra")]
+        Quadra      = 28,
+        [Description("Recanto")]
+        Recanto     = 29,
+        [Description("Residencial")]
+        Residencial = 30,
+        [Description("Rodovia")]
+        Rodovia     = 31,
+        [Description("Rua")]
+        Rua         = 32,
+        [Description("Setor")]
+        Setor       = 33,
+        [Description("Sítio")]
+        Sitio       = 34,
+        [Description("Travessa")]
+        Travessa    = 35,
+        [Description("Trecho")]
+        Trecho      = 36,
+        [Description("Trevo")]
+        Trevo       = 37,
+        [Description("Vale")]
+        Vale        = 38,
+        [Description("Vereda")]
+        Vereda      = 39,
+        [Description("Via")]
+        Via         = 40,
+        [Description("Viaduto")]
+        Viaduto     = 41,
+        [Description("Viela")]
+        Viela       = 42,
+        [Description("Vila")]
+        Vila        = 43,
+        [Description("Outros")]
+        Outros      = 44
+    }
+}
